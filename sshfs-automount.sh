94f6501053b7cc4a5f5c2a0fb043ab482ca5d697
George G <kreach3r@users.noreply.github.com>
2016-05-08 21:39:17 +0300

    Fixes everywhere, 0.2
    - Moved functions above the running code (bash limitation, it seems, commands couldn't be found)
    - Fixed typo #sshprivatekey to $sshprivatekey
    - Added more dependencies
    - Made mount command more bulletproof (it forces an unmount before trying to mount again)
    - More code syntax error

--- sshfs-automount.sh
+++ sshfs-automount.sh
@@ -2,7 +2,9 @@
 # Automount sshfs volume automatically
 
 # Dependencies
+# sudo apt-get install sshfs
 # sudo apt-get install notify-osd
+# sudo apt-get install libnotify-bin
 
 # internal variables
 t=1
@@ -12,27 +14,8 @@ s=1
 # Load server variables
 source ~/.sshfs-server.cfg
 
-#Start of the script
-
-echo "Starting script..."
-until [ $s = 0 ]; do # check for the sshfs mount
-    #Count to 120s and ping the server until the server is online
-    until [ $t = 0 ]; do
-        min=$(( $interval/60 ))
-        echo "waiting $min minutes..."
-        sleep $interval
-        echo "Pinging the host..."
-        ping -c 2 $host
-        t=$? # setting the t variable to the ping error code, so that the loop continues. It's 0 only when it is successful.
-    done
-    sleep 5 # just for the sake of it
-    echo "Trying to mount the remote sshfs volume..."
-    sshfs_mount
-    notify $s # notify about the result
-done
-
 #functions
-function notify {
+function notify() {
     if [ "$1" = 1 ]; then
         while [ $c = 0 ]; do
             notify-send "SSHFS" "There was a problem mounting the SSHFS volume. Check if the server is running Linux and/or if the mount is already mounted."
@@ -49,7 +32,28 @@ function notify {
     fi
 }
  
-function sshfs_mount {
-    sshfs $user@$host:/ $mountpoint -o IdentityFile=#sshprivatekey -o idmap=user -p $port
+function sshfs_mount() {
+    # Unmount local mountpoint first (this solves "endpoint not connected" error)
+    fusermount -uz $mountpoint
+    sshfs $user@$host:/ $mountpoint -o IdentityFile=$sshprivatekey -o idmap=user -p $port
     s=$?
 }
+
+# Start of the script
+
+echo "Starting sshfs-automount..."
+until [ $s = 0 ]; do # check for the sshfs mount
+    # Count to 120s and ping the server until the server is online
+    until [ $t = 0 ]; do
+        min=$(( $interval/60 ))
+        echo "waiting $min minutes..."
+        sleep $interval
+        echo "Pinging the host..."
+        ping -c 2 $host
+        t=$? # setting the t variable to the ping error code, so that the loop continues. It's 0 only when it is successful.
+    done
+    sleep 5 # just for the sake of it
+    echo "Trying to mount the remote sshfs volume..."
+    sshfs_mount
+    notify $s # notify about the result
+done
